91b5e07fea67a1ee1cb6f65e3f2d4544d54f8be2
moustcamara <moust@moustcamara.com>
2021-05-10 21:59:52 -0400

    Updated main page

--- src/App.js
+++ src/App.js
@@ -36,11 +36,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
+          <h1>
+           Lyric Boost
+          </h1>
         </header>
       </div>
     )
